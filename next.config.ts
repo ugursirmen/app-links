3f6c3b6484312c41744c8adeacc8df92e8766cd1
ugursirmen <ugur.sirmen@vagani.com>
2025-10-26 13:54:49 +0300

    init

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          { key: 'Content-Type', value: 'application/json' }
+        ],
+      },
+      {
+        source: '/.well-known/assetlinks.json',
+        headers: [
+          { key: 'Content-Type', value: 'application/json' }
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
